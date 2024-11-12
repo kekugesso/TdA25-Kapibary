17a7712c04fd1a8c7bf2c7459ce384d4b04bef1f
kekugesso <vitalymir16@gmail.com>
2024-11-12 12:21:53 +0100

    Special fix for tourdeapp

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("games", views.FrontForFun),
-    path("api/games", views.AllGamesView.as_view()),
-    path("api/games/<uuid1>", views.GameView.as_view()),
+    path("game", views.FrontForFun),
+    path("api/v1/games", views.AllGamesView.as_view()),
+    path("api/v1/games/<uuid1>", views.GameView.as_view()),
 ]
