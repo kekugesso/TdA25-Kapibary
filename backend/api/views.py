6c14724df806f9832f47fd4d7a4678d669a15bd1
kekugesso <vitalymir16@gmail.com>
2024-11-26 07:22:24 +0100

    Add function for control gameState endgame, and add some status codes in api/v1/games POST and api/v1/games/<uuid> PUT

--- backend/api/views.py
+++ backend/api/views.py
@@ -23,21 +23,39 @@ class AllGamesView(APIView):
 
     def post(self, request):
         data = request.data
-        data["uuid"] = str(uuid.uuid4())
-        data["gameState"] = "unknown"
+        countx = 0
+        counto = 0
         board = data.pop('board')
+        if len(board) != 15 or len(board[0]) != 15:
+            return Response({"message": "Invalid board size"}, status=422)
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] != "":
+                    if board[i][j] == "X":
+                        countx += 1
+                    elif board[i][j] == "O":
+                        counto += 1
+        if countx - counto < 0 or countx - counto > 1:
+            return Response({"message": "Invalid number of X and O"}, status=422)
+        if counto >= 5:
+            data["gameState"] = "midgame"
+        else:
+            data["gameState"] = "opening"
+        if can_win_next_move(board, "X") or can_win_next_move(board, "O"):
+            data["gameState"] = "endgame"
+        data["uuid"] = str(uuid.uuid4())
         serializer = GameSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
         else:
             return Response(serializer.errors, status=400)
-        if (len(board) != 15 or len(board[0]) != 15):
-            game = Game.objects.get(uuid=data["uuid"])
-            game.delete()
-            return Response({"message": "Invalid board size"}, status=422)
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] != "":
+                    if board[i][j] == "X":
+                        countx += 1
+                    elif board[i][j] == "O":
+                        counto += 1
                     data_board = {"uuid": str(uuid.uuid4()),"row": i, "column": j, "symbol": board[i][j], "game": data["uuid"]}
                     serializer_board = BoardSerializer(data=data_board)
                     if serializer_board.is_valid():
@@ -73,15 +91,21 @@ class GameView(APIView):
         game = get_object_or_404(Game, uuid=uuid1)
         boards = Board.objects.all().filter(game=uuid1)
         data = request.data
+        countx = 0
+        counto = 0
         data["uuid"] = uuid1
-        board = data.pop('board')
+        board_main = data.pop('board')
         cells = []
-        if (len(board) != 15 or len(board[0]) != 15):
+        if (len(board_main) != 15 or len(board_main[0]) != 15):
             return Response({"message": "Invalid board size"}, status=422)
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] != "":
-                    data_board = {"uuid": str(uuid.uuid4()),"row": i, "column": j, "symbol": board[i][j], "game": uuid1}
+        for i in range(len(board_main)):
+            for j in range(len(board_main[0])):
+                if board_main[i][j] != "":
+                    if board_main[i][j] == "X":
+                        countx += 1
+                    elif board_main[i][j] == "O":
+                        counto += 1
+                    data_board = {"uuid": str(uuid.uuid4()),"row": i, "column": j, "symbol": board_main[i][j], "game": uuid1}
                     serializer_board = BoardSerializer(data=data_board)
                     if serializer_board.is_valid():
                         cells.append(data_board)
@@ -93,6 +117,16 @@ class GameView(APIView):
             serializer_board = BoardSerializer(data=cell)
             if serializer_board.is_valid():
                 serializer_board.save()
+        if countx - counto < 0 or countx - counto > 1:
+            return Response({"message": "Invalid number of X and O"}, status=422)
+        if counto >= 5:
+            data["gameState"] = "midgame"
+        else:
+            data["gameState"] = "opening"
+        print(can_win_next_move(board_main, "X"))
+        print(can_win_next_move(board_main, "O"))
+        if can_win_next_move(board_main, "X") or can_win_next_move(board_main, "O"):
+            data["gameState"] = "endgame"
         serializer = GameSerializer(game, data=data)
         if serializer.is_valid():
             serializer.save()
@@ -112,3 +146,37 @@ def FrontForFun(request):
 
 def SpecificFrontForFun(request, uuid1):
     return HttpResponse("Specific front for fun", content_type="text/html")
+
+def can_win_next_move(board, symbol):
+    size = 15
+    for row in range(size):
+        for col in range(size):
+            # Пропустить занятые ячейки
+            if board[row][col] == "":
+                continue
+
+            # Проверить горизонталь
+            if col <= size - 5:
+                line = [board[row][col + i] for i in range(5)]
+                if all(cell in {symbol, ""} for cell in line) and line.count(symbol) == 4:
+                    return True
+
+            # Проверить вертикаль
+            if row <= size - 5:
+                line = [board[row + i][col] for i in range(5)]
+                if all(cell in {symbol, ""} for cell in line) and line.count(symbol) == 4:
+                    return True
+
+            # Проверить диагональ слева направо
+            if row <= size - 5 and col <= size - 5:
+                line = [board[row + i][col + i] for i in range(5)]
+                if all(cell in {symbol, ""} for cell in line) and line.count(symbol) == 4:
+                    return True
+
+            # Проверить диагональ справа налево
+            if row <= size - 5 and col >= 4:
+                line = [board[row + i][col - i] for i in range(5)]
+                if all(cell in {symbol, ""} for cell in line) and line.count(symbol) == 4:
+                    return True
+
+    return False
