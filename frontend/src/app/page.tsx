5a00ea521c22a5c306ccba12836f6751d7b7e124
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-12-24 00:28:32 +0100

    feat: added about us section on home page
    feat: changed page layout: <section> -> <article>
    
    feat: added mobile support

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,17 +13,17 @@ export default function Home() {
   const [buttonClick, setButtonClick] = useState(false);
 
   return (
-    <main>
-      <div
+    <section>
+      <article
         id="greater"
         className="flex flex-col flex-center h-[calc(100dvh-var(--navbar-height))] text-center text-balance"
       >
-        <h1 className="font-semibold text-5xl mb-5 max-w-[60%] leading-[60px]">
+        <h1 className="font-semibold text-5xl mb-5 px-4 md:max-w-[60%] leading-[60px]">
           {buttonClick
             ? "Jste připraveni ponořit se do hry?"
             : "Rozvíjejte svou mysl s piškvorkovými hádankami"}
         </h1>
-        <p className="font-medium text-xl max-w-[70%] leading-[25px]">
+        <p className="font-medium text-xl px-4 md:max-w-[70%] leading-[25px]">
           {buttonClick
             ? "Vyberte si, kam chcete pokračovat. Můžete zahájit novou hru a rovnou se pustit do akce, nebo si prohlédnout seznam zajímavých rozehraných her. Stačí si vybrat!"
             : "Vítejte na inovativní platformě Think different Academy, kde se zábava setkává se strategií! Zapojte se do vzrušujících piškvorkových hádanek, jejichž cílem je zdokonalit vaše logické a taktické myšlení."}
@@ -35,13 +35,13 @@ export default function Home() {
             setIsClicked={setButtonClick}
           />
         </div>
-      </div>
-      <div
+      </article>
+      <article
         id="about-site"
-        className="flex flex-center text-center text-balance space-x-5 p-16"
+        className="flex flex-col flex-center gap-5 p-4 text-center md:text-left text-balance md:p-16 lg:px-32 sm:flex-row"
       >
-        <div className="flex flex-col flex-center text-balance text-left">
-          <h1 className="font-semibold text-3xl mb-5 leading-9">
+        <div>
+          <h1 className="font-semibold text-3xl mb-5 leading-9 text-center md:text-left ">
             Prozkoumejte naše poutavé hádanky a bavte se!
           </h1>
           <p>
@@ -55,12 +55,12 @@ export default function Home() {
           alt="Site picture"
           height={500}
           width={500}
-          className="bg-white-darker min-h-[500px] min-w-[500px]"
-          loading="eager"
+          // loading="eager"
+          className="flex flex-center text-gray-500 bg-white-darker min-h-[300px] min-w-[300px] md:min-h-[500px] md:min-w-[500px]"
         />
-      </div>
-      <div id="site-features">
-        <div className="flex flex-col flex-center text-center text-balance pt-24 px-24">
+      </article>
+      <article id="site-features">
+        <div className="flex flex-col flex-center text-center text-balance p-4 md:pt-24 md:px-24">
           <h1 className="font-semibold text-3xl mb-5 leading-9">
             Připojte se k zábavě a vyzvěte svou mysl
           </h1>
@@ -71,9 +71,30 @@ export default function Home() {
           </p>
         </div>
         <SiteFeatures />
-      </div>
-      <div id="about-us"></div>
-    </main>
+      </article>
+      <article id="about-us" className="flex flex-col flex-center">
+        <Image
+          src="/img/logo-large.png"
+          alt="Site logo"
+          height={300}
+          width={300}
+          // loading="eager"
+          className="flex flex-center text-gray-500 min-h-[100px] min-w-[100px] md:min-h-[300px] md:min-w-[300px]"
+        />
+        <div className="flex flex-col gap-4 text-center p-4 md:text-left md:grid md:grid-cols-2 md:p-24">
+          <h1 className="flex flex-center font-semibold text-2xl text-center md:text-left">
+            Posilte svou mysl pomocí piškvorek
+          </h1>
+          <p>
+            Naše platforma nabízí dětem a mládeži zábavný a poutavý způsob, jak
+            zlepšit jejich logické a taktické myšlení. Řešením piškvorek mohou
+            uživatelé v hravém prostředí rozvíjet schopnosti kritického řešení
+            problémů. Připojte se k nám a podporujte kreativitu a strategické
+            myšlení prostřednictvím interaktivní hry.
+          </p>
+        </div>
+      </article>
+    </section>
   );
 }
 
@@ -130,28 +151,28 @@ function GameButton({
   );
 }
 
-interface FeatureItem {
-  readonly img: string;
+type FeatureItem = {
+  readonly img: { src: string; alt: string };
   readonly title: string;
   readonly detail: string;
-}
+};
 
 function SiteFeatures() {
   const items: FeatureItem[] = [
     {
-      img: "/img/f1", // Example image source
+      img: { src: "/img/f1", alt: "Bulb Think" },
       title: "Výzvy pro začátečníky i experty",
       detail:
         "Od jednoduchých her pro nováčky až po strategické pro experty – zvolte si svou obtížnost.",
     },
     {
-      img: "/img/f2",
+      img: { src: "/img/f2", alt: "Bulb Play" },
       title: "Hrajte proti přátelům na jednom zařízení",
       detail:
         "Užijte si lokální multiplayer a soutěžte s přáteli v piškvorkách na jednom zařízení.",
     },
     {
-      img: "/img/f3",
+      img: { src: "/img/f3", alt: "Bulb Idea" },
       title: "Hrajte a zdokonalujte své dovednosti",
       detail:
         "Ponořte se do světa piškvorkových hlavolamů ještě dnes a posouvejte své schopnosti na novou úroveň.",
@@ -159,20 +180,20 @@ function SiteFeatures() {
   ];
 
   return (
-    <div className="flex flex-row justify-stretch justify-items-stretch gap-6 p-6">
+    <div className="flex flex-col justify-stretch justify-items-stretch gap-6 p-4 md:p-16 text-balance text-center sm:flex-row">
       {items.map((item, index) => (
         <div key={index} className="flex-1 flex flex-col items-center p-4">
           <Image
-            src={item.img}
-            alt={item.title}
+            {...item.img}
             width={50}
             height={50}
-            className=""
+            className="flex flex-center text-gray-500"
+            /* loading="eager" */
           />
           <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-100 mb-2">
             {item.title}
           </h3>
-          <p className="text-gray-600 dark:text-gray-400 text-sm text-center">
+          <p className="text-gray-600 dark:text-gray-400 text-sm">
             {item.detail}
           </p>
         </div>
