22fe1148bb2dc3f00c607925285835458842dd23
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-12-27 10:04:11 +0100

    feat: added first board component implementation

---
+++ frontend/src/components/game/Board.tsx
@@ -0,0 +1,29 @@
+type BoardProps = {
+  board: string[][];
+  handleClick: (x: number, y: number) => void;
+  buttonClass?: string;
+};
+
+export default function Board({
+  board,
+  handleClick,
+  buttonClass = "w-16 h-16 border border-black",
+}: BoardProps) {
+  return (
+    <div className="flex flex-col items-center">
+      {board.map((row, y) => (
+        <div key={y} className="flex">
+          {row.map((cell, x) => (
+            <button
+              key={x}
+              className={buttonClass}
+              onClick={() => handleClick(x, y)}
+            >
+              {cell}
+            </button>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
