2a0313042bef1a609f1681cc94a793a70ec1989e
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-12-29 13:43:32 +0100

    feat: added logo on about us on home page

--- frontend/src/components/home/AboutUs.tsx
+++ frontend/src/components/home/AboutUs.tsx
@@ -1,15 +1,24 @@
+"use client";
+
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export default function AboutUs() {
+  const { theme } = useTheme();
+
   return (
     <article id="about-us" className="flex flex-col flex-center">
       <Image
-        src="/img/logo-large.png"
+        src={
+          theme == "light"
+            ? "/img/logo_blue_large_black.svg"
+            : "/img/logo_blue_large_white.svg"
+        }
         alt="Site logo"
         height={300}
-        width={300}
-        // loading="eager"
-        className="flex flex-center text-gray-500 min-h-[100px] min-w-[100px] md:min-h-[300px] md:min-w-[300px]"
+        width={400}
+        loading="eager"
+        className="flex flex-center text-gray-500 min-h-[100px] min-w-[100px] md:min-h-[300px] md:min-w-[400px] p-4"
       />
       <div className="flex flex-col gap-4 text-center p-4 md:text-left md:grid md:grid-cols-2 md:p-24">
         <h1 className="flex flex-center font-semibold text-2xl text-center md:text-left">
