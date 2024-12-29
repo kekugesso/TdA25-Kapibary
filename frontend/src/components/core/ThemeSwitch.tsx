4fb5301637879b038117a0cb3e1b0fe5ceb05366
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-12-29 13:56:46 +0100

    feat: first implementation of themeswitch

--- frontend/src/components/core/ThemeSwitch.tsx
+++ frontend/src/components/core/ThemeSwitch.tsx
@@ -1,25 +1,55 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function ThemeSwitch() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
+  // Ensure the component is only rendered on the client side
   useEffect(() => {
     setMounted(true);
   }, []);
 
+  // Handle cases where the component renders before hydration
+  if (!mounted) return null;
+
+  const isDark = theme === "dark";
+
   return (
-    <button
-      onClick={() => {
-        if (!mounted) location.reload();
-        else setTheme(theme === "light" ? "dark" : "light");
-      }}
-      className="text-lg font-semibold text-gray-800 hover:text-gray-900"
-    >
-      {theme === "light" ? "🌞" : "🌚"}
-    </button>
+    <div className="flex flex-center">
+      <input
+        type="checkbox"
+        checked={isDark}
+        onChange={() =>
+          setTheme((prev) => (prev === "light" ? "dark" : "light"))
+        }
+        id="theme-switch"
+        aria-label={`Switch to ${isDark ? "light" : "dark"} mode`}
+        className="hidden"
+      />
+      <label
+        htmlFor="theme-switch"
+        className="relative cursor-pointer flex justify-items-center justify-between bg-white rounded-full"
+      >
+        <Image
+          src="/img/moon.svg"
+          alt="Moon Icon"
+          width={20}
+          height={20}
+          className="m-2"
+        />
+        <span className="bg-white border-black absolute top-[5%] left-[4%] border-2 w-8 h-8 rounded-full transition-all duration-300 dark:translate-x-[105%]" />
+        <Image
+          src="/img/sun.svg"
+          alt="Sun Icon"
+          width={20}
+          height={20}
+          className="m-2"
+        />
+      </label>
+    </div>
   );
 }
