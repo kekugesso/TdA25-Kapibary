e32e97705b56edf3cf68cc2d0f60d2d508b909b8
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-12-31 23:48:30 +0100

    feat(components): added dynamic scaling to board component

--- frontend/src/components/game/Board.tsx
+++ frontend/src/components/game/Board.tsx
@@ -4,19 +4,27 @@ type BoardProps = {
   buttonClass?: string;
 };
 
+// When adding support for different board sizes, add the size for tailwind to generate the classes
+const generate_classes = [
+  "[--board-size:5]",
+  "[--board-size:10]",
+  "[--board-size:15]",
+];
+
 export default function Board({
   board,
   handleClick,
-  buttonClass = "w-16 h-16 border border-black",
+  buttonClass = "[--size:calc(99vmin/var(--board-size))] sm:[--size:5vmin] w-[--size] h-[--size]",
 }: BoardProps) {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center rounded-lg border-white border overflow-hidden">
       {board.map((row, y) => (
         <div key={y} className="flex">
           {row.map((cell, x) => (
             <button
               key={x}
-              className={`${buttonClass} ${x % 2 == 0 && "bg-black"}`}
+              data-position={`${y},${x}`}
+              className={`${buttonClass} border-white ${(x + y) % 2 == 0 ? "bg-black" : ""} ${x != row.length - 1 ? "border-r" : ""} ${y != board.length - 1 ? "border-b" : ""} [--board-size:${board.length}]`}
               onClick={() => handleClick(x, y)}
             >
               {cell}
