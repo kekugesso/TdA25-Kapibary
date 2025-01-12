fd41c5ad058d99096fa7c2defd1818a16fff65c2
Vojtěch <149375622+VSladek@users.noreply.github.com>
2025-01-12 22:00:42 +0100

    feat: added working search implementation
    feat: used new components in games

--- frontend/src/app/games/page.tsx
+++ frontend/src/app/games/page.tsx
@@ -2,7 +2,7 @@
 
 import { GameData } from "@/types/games/GameData";
 import Loading from "@/components/core/Loading";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import {
   Modal,
@@ -10,31 +10,82 @@ import {
   ModalFooter,
   ModalHeader,
 } from "@/components/core/Modal";
-import { DifficultyToString } from "@/types/search/difficulty";
 import { useEffect, useState } from "react";
-import {
-  UpdatedAtFromStringDate,
-  UpdatedAtToString,
-} from "@/types/search/updatedAt";
-import Image from "next/image";
+import Card from "@/components/games/Card";
+import SearchBar from "@/components/games/SearchBar";
+import { SearchQueryType } from "@/types/search/SearchQuery";
+import { updatedAt } from "@/types/search/updatedAt";
+import { difficulty } from "@/types/search/difficulty";
 
-export default function Game() {
+export default function Games() {
   const router = useRouter();
   const [games, setGames] = useState<GameData[]>([]);
+  const [isBlocked, setIsBlocked] = useState(false);
 
-  const { isPending, error, data } = useQuery({
-    queryKey: ["games"],
-    queryFn: () =>
-      fetch(`/api/games`)
+  // if we want to set a function it has to be a function that returns a function
+  const [filterFunction, setFilterFunction] = useState<
+    (data: GameData[]) => GameData[]
+  >(() => (data: GameData[]) => data);
+
+  const [filterParams, setFilterParams] = useState<SearchQueryType>({
+    difficulty: [
+      difficulty.beginner,
+      difficulty.easy,
+      difficulty.medium,
+      difficulty.hard,
+      difficulty.extreme,
+    ],
+    updatedAt: updatedAt["3months"],
+  });
+
+  useEffect(() => {
+    if (!window) return;
+    if (isBlocked) return;
+    getGames();
+  }, [filterParams, isBlocked]);
+
+  const {
+    mutate: getGames,
+    isPending,
+    error,
+    data,
+  } = useMutation({
+    mutationKey: ["games"],
+    mutationFn: () =>
+      fetch(`/api/filter`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(filterParams),
+      })
         .then((res) => res.json())
         .then((data) => data as GameData[]),
   });
 
   useEffect(() => {
-    if (data) {
-      setGames(data);
-    }
-  }, [data]);
+    if (data) setGames(filterFunction(data));
+  }, [data, filterFunction]);
+
+  const handleSearch = (search: string, filter: SearchQueryType) => {
+    setFilterParams(filter);
+
+    // simple filter that checks if the name of the game includes the search string
+    setFilterFunction(
+      () => (data: GameData[]) =>
+        data.filter((game) =>
+          game.name.toLowerCase().includes(search.toLowerCase()),
+        ),
+    );
+  };
+
+  // // console function for search testing
+  // useEffect(() => {
+  //   if (window)
+  //     window.manualSearch = function (search: string) {
+  //       handleSearch(search, filterParams);
+  //     };
+  // }, [filterParams]);
 
   const handleClose = () => router.back();
 
@@ -43,38 +94,30 @@ export default function Game() {
       {isPending && games ? (
         <Loading />
       ) : (
-        <article className="flex flex-row sm:flex-row justify-around content-around flex-wrap">
-          {games &&
-            games.map((game) => (
-              <div
-                key={game.uuid}
-                className="flex flex-col bg-black rounded-lg items-center justify-between h-fit overflow-hidden w-[280px]"
-              >
-                <div className="flex justify-between w-full h-fit bg-blue-light dark:bg-blue-dark">
-                  <p className="p-3 text-2xl font-bold">{game.name}</p>
-                  <Image
-                    src={`/img/level_${game.difficulty}.svg`}
-                    alt={game.name}
-                    width={40}
-                    height={40}
-                    className="mx-3"
-                  />
-                </div>
-
-                <p className="p-2 text-xl font-semibold self-start">
-                  {DifficultyToString(game.difficulty)}
-                </p>
-                <p className="p-2 text-xl font-semibold self-start">
-                  {UpdatedAtToString(UpdatedAtFromStringDate(game.updatedAt))}
-                </p>
-                <button
-                  onClick={() => router.push(`/game/${game.uuid}`)}
-                  className="w-[90%] m-2 text-xl bg-blue-light dark:bg-blue-dark text-white dark:text-black font-semibold rounded-lg py-2 px-6"
-                >
-                  Hrat
-                </button>
-              </div>
-            ))}
+        <article className="flex flex-col gap-4">
+          <div className="flex justify-center m-5">
+            <div className="w-[80vw]">
+              <SearchBar
+                searchAction={handleSearch}
+                filterParams={filterParams}
+                filterSetAction={setFilterParams}
+                filterBlockAction={setIsBlocked}
+                applyFilterAction={getGames}
+              />
+            </div>
+          </div>
+          <div
+            className="flex flex-wrap justify-center gap-10 mb-10"
+            aria-label="Games"
+          >
+            {games ? (
+              games.map((game) => <Card key={game.uuid} game={game} />)
+            ) : (
+              <p className="self-center text-center text-balance font-medium">
+                No games found.
+              </p>
+            )}
+          </div>
         </article>
       )}
       {error && (
