a580b7d3ec15ec5fe5e2a0116103643996241f57
Vojtěch <149375622+VSladek@users.noreply.github.com>
2025-02-24 12:32:01 +0100

    feat: update user type

--- frontend/src/types/auth/user.tsx
+++ frontend/src/types/auth/user.tsx
@@ -1,5 +1,6 @@
 type User = {
   uuid: string;
+  avatar: string;
   elo: number;
   createdAt: string;
   username: string;
@@ -10,4 +11,41 @@ type User = {
   draws: number;
 };
 
-export type { User };
+type SmallUser = {
+  username: string;
+  elo: number;
+};
+
+enum GameResult {
+  UNKNOWN = "unknown",
+  WIN = "win",
+  LOSE = "lose",
+  DRAW = "draw",
+}
+
+type GameHistory = {
+  uuid: string;
+  createdAt: string;
+  elo: number;
+  elo_change: number;
+  symbol: "X" | "O";
+  result: GameResult;
+  opponent: SmallUser;
+};
+
+type UserSettings = {
+  username?: string;
+  email?: string;
+  password?: string;
+  avatar?: string;
+};
+
+type UserSettingsError = {
+  username?: string[];
+  email?: string[];
+  password?: string[];
+  avatar?: string[];
+};
+
+export type { User, SmallUser, GameHistory, UserSettings, UserSettingsError };
+export { GameResult };
