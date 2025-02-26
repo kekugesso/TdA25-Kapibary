e1c736d8318379ed0a7e67fe0784b51388dffc60
Vojtěch <149375622+VSladek@users.noreply.github.com>
2025-02-26 23:05:19 +0100

    feat: added styling for leaderboard page

--- frontend/src/app/leaderboard/page.tsx
+++ frontend/src/app/leaderboard/page.tsx
@@ -3,6 +3,7 @@ import PagedItems from "@/components/core/pagination/PagedItems";
 import PageSelector from "@/components/core/pagination/PageSelector";
 import Pagination from "@/components/core/pagination/Pagination";
 import { User } from "@/types/auth/user";
+import Image from "next/image";
 
 export default function Leaderboard() {
   const fetchItems = async ({
@@ -24,11 +25,39 @@ export default function Leaderboard() {
       <article className="flex flex-col">
         <PagedItems<User>>
           {(items) => (
-            <ul className="divide-y">
-              {items.map((item) => (
-                <li key={item.uuid} className="p-2">
-                  {item.username}: {item.elo}
-                </li>
+            <ul className="p-[5%] space-y-2">
+              {items.map((item, index) => (
+                <>
+                  {index === 0 && (
+                    <div className="p-2 -mb-3 grid grid-cols-[7.5%,17%,18%,15%,35%,7.5%] text-xl text-center items-center">
+                      <span>Pořadí</span>
+                      <span>Uživatel</span>
+                      <span />
+                      <span>Výhry/Prohry/Remízy</span>
+                      <span />
+                      <span>Elo</span>
+                    </div>
+                  )}
+                  <li
+                    key={item.uuid}
+                    className="p-2 grid grid-cols-[7.5%,7.5%,30%,10%,37.5%,7.5%] text-center items-center font-bold text-3xl bg-black rounded-lg"
+                  >
+                    <span>{item.position || index}.</span>
+                    <span className="flex flex-center">
+                      <Image
+                        src={item.avatar || "/img/avatar.svg"}
+                        alt={item.username}
+                        width={64}
+                        height={64}
+                        className="rounded-lg bg-white"
+                      />
+                    </span>
+                    <span className="text-start">{item.username}</span>
+                    <span>{`${item.wins}/${item.draws}/${item.losses}`}</span>
+                    <span />
+                    <span>{item.elo}</span>
+                  </li>
+                </>
               ))}
             </ul>
           )}
